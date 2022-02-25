bc409136dabf0422fc52023340375315b8ee6f8f
kitakou0313 <kitakou0313@gmail.com>
2022-02-25 09:00:49 +0000

    Add code for unit vec

--- 2/code.py
+++ 2/code.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.linalg import norm
-
 if __name__ == "__main__":
     # ユークリッドノルムの実装
 
@@ -23,3 +22,23 @@ if __name__ == "__main__":
     # axisは適用する軸を指定するイメージ
     a_n = norm(a, axis=1)
     print(a_n)
+
+    v = np.array(
+        [1, 1]
+    )
+    u = v / norm(v)
+    print(u)
+
+    a = np.array(
+        [[1, 2, 3],
+         [4, 5, 6]]
+    )
+
+    a = a / norm(a, axis=1, keepdims=True)
+    print(a)
+
+    a_norm = norm(
+        a, axis=1, keepdims=True
+    )
+
+    print(a_norm)
