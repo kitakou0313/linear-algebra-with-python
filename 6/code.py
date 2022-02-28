2afb99449d19b7b63ba05b5c54ae01715c389593
kitakou0313 <kitakou0313@gmail.com>
2022-02-28 14:12:35 +0000

    Add det transpose

--- 6/code.py
+++ 6/code.py
@@ -10,3 +10,13 @@ if __name__ == "__main__":
 
     det_x = det(x)
     print(det_x)
+
+    # 行列の転置普遍性
+    x = np.array(
+        [[5, 9, 2, 7],
+         [1, 3, 9, 5],
+         [6, 2, 0, 4],
+         [2, 6, 3, 3]]
+    )
+    print(det(x))
+    print(det(x.T))
