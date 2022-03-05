cd65d2b339b47d114dfd04843bc81d8eff0ac188
kitakou0313 <kitakou0313@gmail.com>
2022-03-05 03:33:50 +0000

    Add code of 15

--- 15/main.py
+++ 15/main.py
@@ -24,4 +24,25 @@ if __name__ == "__main__":
     pointer(ax, p1[0], p1[1], "P1",
             pcolor="blue", textsize=15)
 
-    plt.savefig("img.png")
+    plt.savefig("1.png")
+
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111)
+
+    coordinate(ax, [-6, 6], [-6, 6])
+
+    a = np.array(
+        [[1, 0],
+         [0, -1]]
+    )
+
+    x = np.linspace(-6, 6, 65)
+    line_0 = np.vstack((x, 2*x))
+    line_1 = np.dot(a, line_0)
+
+    ax.plot(line_0[0], line_0[1], color="red", label="y = 2x")
+    ax.plot(line_1[0], line_1[1], color="blue", label="y = -2x")
+
+    ax.legend()
+
+    plt.savefig("2.png")
